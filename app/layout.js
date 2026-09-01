a7e8ffdfaaea12743c94320e6449e5c06e423d0e
Ali Farzalisoy <alifarzalisoy099@gmail.com>
2026-09-01 17:16:21 +0400

    Update favicon and Google verification

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,8 @@ export const metadata = {
     google: "DhuRRBhIM2rtc37SheTfQnsql1vEMrTk8Hsv723mRO0",
   },
   icons: {
-    icon: '/soilcreditfavicon.png',
-    shortcut: '/soilcreditfavicon.png',
+    icon: '/soilfavicon.png',
+    shortcut: '/soilfavicon.png',
   },
 };
 
